92a969456aefe15d862c54895b9b7ad37d9c25e0
Jan Graichen <jan.graichen@altimos.de>
2012-05-10 18:24:01 +0200

    Bump version to rc3.

--- lib/rails-timeago/version.rb
+++ lib/rails-timeago/version.rb
@@ -4,7 +4,7 @@ module Rails
       MAJOR = 1
       MINOR = 2
       PATCH = 0
-      STAGE = 'rc2'
+      STAGE = 'rc3'
 
       def self.to_s
         [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join '.'
