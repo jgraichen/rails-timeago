3b909d5085b441250695c5277439c861a0bb70d6
Jan Graichen <jan.graichen@altimos.de>
2012-05-25 13:09:18 +0200

    Release version 1.3.0 with tested i18n support.

--- lib/rails-timeago/version.rb
+++ lib/rails-timeago/version.rb
@@ -2,9 +2,9 @@ module Rails
   module Timeago
     module VERSION
       MAJOR = 1
-      MINOR = 2
+      MINOR = 3
       PATCH = 0
-      STAGE = 'rc3'
+      STAGE = nil
 
       def self.to_s
         [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join '.'
