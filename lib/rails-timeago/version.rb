bafa1b68fd8696e83a6e8acec70050618014d377
Jan Graichen <jg@altimos.de>
2012-11-24 15:20:34 +0100

    Bump version to 2.0.0.beta1 due to incompatible changes with locale handling.

--- lib/rails-timeago/version.rb
+++ lib/rails-timeago/version.rb
@@ -1,10 +1,10 @@
 module Rails
   module Timeago
     module VERSION
-      MAJOR = 1
-      MINOR = 4
-      PATCH = 3
-      STAGE = nil
+      MAJOR = 2
+      MINOR = 0
+      PATCH = 0
+      STAGE = 'beta1'
 
       def self.to_s
         [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join '.'
