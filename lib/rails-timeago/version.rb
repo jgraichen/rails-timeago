305ee002ad7734303d5a0ee68a6222c619c9fc19
Jan Graichen <jg@altimos.de>
2013-01-08 22:09:09 +0100

    Bump version.

--- lib/rails-timeago/version.rb
+++ lib/rails-timeago/version.rb
@@ -2,9 +2,9 @@ module Rails
   module Timeago
     module VERSION
       MAJOR = 2
-      MINOR = 0
+      MINOR = 1
       PATCH = 0
-      STAGE = 'beta1'
+      STAGE = nil
 
       def self.to_s
         [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join '.'
