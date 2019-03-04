5190bec583d65fdc23d42260bfcd52c4a4ce11b5
Jan Graichen <jgraichen@altimos.de>
2019-03-04 14:13:27 +0100

    Use webrick as capybara server
    Capybara 3 tries to use puma as default which is not available here.
    Instead of adding another dependency we'll use webrick instead.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,9 @@ require 'capybara/poltergeist'
 require File.dirname(__FILE__) + '/../lib/rails-timeago.rb'
 require File.dirname(__FILE__) + '/support/stub.rb'
 
-Capybara.javascript_driver = :poltergeist
 Capybara.app = Application.instance
+Capybara.javascript_driver = :poltergeist
+Capybara.server = :webrick
 
 RSpec.configure do |config|
   config.mock_with :rspec
